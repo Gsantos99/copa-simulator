7c442d167d47f38342674e0eab408a809d4be318
Giulyano Santos <giulyanofelipesantos@gmail.com>
2022-10-15 15:14:11 -0300

    Atualizando JS

--- src/main.js
+++ src/main.js
@@ -4,7 +4,5 @@ import './modules/gerador'
 import { Copa } from './modules/gerador';
 import './modules/paginas'
 
-console.log('Main.js ON')
 
-let copa = new Copa;
-copa.geraGrupos();
+
--- src/modules/gerador.js
+++ src/modules/gerador.js
@@ -1,14 +1,56 @@
-//Classe e métodos aqui!
 
-export {Copa} 
+export { Copa }
 
-
+const url = 'https://estagio.geopostenergy.com/WorldCup/GetAllTeams'
 
 
 
 class Copa {
-  geraGrupos() {
-    console.log('Gera Grupos');
+  // 1º Consume Api
+  getSelecoes(url) {
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'git-user': 'Gsantos99'
+      }
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+        console.log('Peguei as seleções ')
+        
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
+
+  //2º Separa os grupos
+
+  getGrupos() {
+    console.log('Peguei os grupos')
+    
+
+  }
+
+  // 3º Realizar partidas
+  getPartida() {
+    console.log('Fazer partidas')
+    
+  }
+
+  // 4º Realizar as rodadas
+  getRodadas() {
+    console.log('Fazer as rodadas')
+    
+  }
+
+  // 5º Contabilizar pontuações
+  getPontuacao() {
+    console.log('Realizar pontuação')
   }
 }
+
+let copa = new Copa()
+copa.getSelecoes(url);
 
